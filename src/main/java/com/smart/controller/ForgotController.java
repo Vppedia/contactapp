27a46ffd0209932f56022d47864f637933a7ecdb
Pandey <vivekpandey9@deloitte.com>
2021-10-30 12:39:49 +0530

    adding comment for git practical demo

--- src/main/java/com/smart/controller/ForgotController.java
+++ src/main/java/com/smart/controller/ForgotController.java
@@ -121,6 +121,8 @@ public class ForgotController {
 		}
 	}
 	
+	
+	//changes done by vivek for git practical
 	//change password
 	@PostMapping("/change-password")
 	public String changePassword(@RequestParam("newpassword") String newpassword,HttpSession session)
