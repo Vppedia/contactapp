48c66c2390326559c2b1ab20f17db1772634b8cb
Vppedia <pandeyvivek.info@gmail.com>
2021-11-14 11:58:12 +0530

    Pleasemove to git

---
+++ src/main/java/com/smart/controller/A.java
@@ -0,0 +1,10 @@
+package com.smart.controller;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/smart/controller/B.java
@@ -0,0 +1,10 @@
+package com.smart.controller;
+
+public class B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
