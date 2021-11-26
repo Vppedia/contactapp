a4b68944d9a5741fa1dcf7ef8ffe497a8d7c32ed
Vppedia <pandeyvivek1810@gmail.com>
2021-11-26 13:15:43 +0530

    line added in pranav in main

--- src/main/java/com/smart/controller/Pranav.java
+++ src/main/java/com/smart/controller/Pranav.java
@@ -12,6 +12,7 @@ public class Pranav
   public static void main(String[] args)
   {
 	  System.out.println("Not Committed the changes!!!!!");
+	  System.out.println("change krke merge krdia");
   }
 
 }
